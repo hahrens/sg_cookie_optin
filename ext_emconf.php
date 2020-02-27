987dff2aadd54946ad8ea78f81eadba1bde58f9a
Tim Wagner <tim.wagner@sgalinski.de>
2020-02-27 22:17:49 +0100

    [TASK] Improve ext_emconf description + buttons in be module
    Resolves #53

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,17 @@
 
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'sgalinski Cookie Optin',
-	'description' => 'This extensions adds a cookie optin for the frontend.',
+	'description' => '
+		This extension adds a customizable cookie optin solution for the frontend including a very basic tag manager.
+		It also allows you to load third party content only when the user allows it.
+		For more details visit https://www.sgalinski.de/en/typo3-products-web-development/cookie-optin-for-typo3/.
+	',
 	'category' => 'module',
 	'version' => '2.0.4',
 	'state' => 'stable',
 	'uploadfolder' => FALSE,
 	'createDirs' => '',
-	'clearcacheonload' => FALSE,
+	'clearCacheOnLoad' => FALSE,
 	'author' => 'Stefan Galinski',
 	'author_email' => 'stefan@sgalinski.de',
 	'author_company' => 'sgalinski Internet Services (https://www.sgalinski.de)',
