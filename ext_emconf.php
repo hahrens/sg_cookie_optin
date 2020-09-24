602ce4a37316a24736e301efcb6f0505c828a195
Georgi Mateev <gmateev@exactag.com>
2020-09-24 22:33:46 +0300

    [FEATURE] Release version 3.2.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
 		For more details visit https://www.sgalinski.de/en/typo3-products-web-development/cookie-optin-for-typo3/.
 	',
 	'category' => 'module',
-	'version' => '3.2.4',
+	'version' => '3.2.5',
 	'state' => 'stable',
 	'uploadfolder' => FALSE,
 	'createDirs' => '',
