d42ab201f364bb06dc723e0aa27bd2e5d20571fd
Georgi Mateev <gmateev@exactag.com>
2021-08-25 13:30:37 +0300

    [BUGFIX] TYPO3 8 Support

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,19 +24,36 @@
  * This copyright notice MUST APPEAR in all copies of the script!
  */
 
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
 call_user_func(
 	static function () {
-		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-			'sg_cookie_optin',
-			'OptIn',
-			[
-                \SGalinski\SgCookieOptin\Controller\OptinController::class => 'show',
-			],
-			// non-cacheable actions
-			[
-                \SGalinski\SgCookieOptin\Controller\OptinController::class => '',
-			]
-		);
+        $currentTypo3Version = VersionNumberUtility::getCurrentTypo3Version();
+        if (version_compare($currentTypo3Version, '11.0.0', '>=')) {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+                'sg_cookie_optin',
+                'OptIn',
+                [
+                    \SGalinski\SgCookieOptin\Controller\OptinController::class => 'show',
+                ],
+                // non-cacheable actions
+                [
+                    \SGalinski\SgCookieOptin\Controller\OptinController::class => '',
+                ]
+            );
+        } else {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+                'SGalinski.sg_cookie_optin',
+                'OptIn',
+                [
+                    'Optin' => 'show',
+                ],
+                // non-cacheable actions
+                [
+                    'Optin' => '',
+                ]
+            );
+        }
 
 		// hook registration
 		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
@@ -66,7 +83,7 @@ call_user_func(
 			\SGalinski\SgCookieOptin\Hook\LicenceCheckHook::class . '->performLicenseCheck';
 
 		// Register Icons
-		if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 7000000) {
+		if ($currentTypo3Version >= 7000000) {
 			$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
 				\TYPO3\CMS\Core\Imaging\IconRegistry::class
 			);
