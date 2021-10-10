dcd82c78f7fa4c1b3b70263964306be7e63a2bd1
Stefan Galinski <stefan@sgalinski.de>
2021-10-10 17:31:53 +0200

    [TASK] Remove PHP constraint (we support all PHP versions that are supported by TYPO3)

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,8 +18,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'sgalinski Internet Services (https://www.sgalinski.de)',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-10.4.99',
-			'php' => '7.0.8-7.4.99',
+			'typo3' => '8.7.0-10.4.99'
 		],
 		'conflicts' => [
 		],
