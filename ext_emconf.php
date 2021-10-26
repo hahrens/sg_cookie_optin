4289a8222e6c0fe081dbc05deeae157f2a3d5501
Georgi Mateev <gmateev@exactag.com>
2021-10-26 18:55:35 +0300

    [TASK] Release version 4.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
 		For more details visit https://www.sgalinski.de/en/typo3-products-web-development/cookie-optin-for-typo3/.
 	',
 	'category' => 'module',
-	'version' => '4.3.0',
+	'version' => '4.3.1',
 	'state' => 'stable',
 	'uploadfolder' => FALSE,
 	'createDirs' => '',
