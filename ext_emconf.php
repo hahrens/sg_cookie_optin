dcbfdbeb39aa1f2b4abc8509e3a82d5379605dc1
Stefan Galinski <stefan@sgalinski.de>
2021-10-29 18:30:22 +0200

    [BUGFIX] Fix TYPO3 constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'sgalinski Internet Services (https://www.sgalinski.de)',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-11.4.99'
+			'typo3' => '8.7.0-11.5.99'
 		],
 		'conflicts' => [
 		],
