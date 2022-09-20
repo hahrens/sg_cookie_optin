53728ebd1588ce0ced0bd04f05d5f8516392f303
Henrik Ahrens <github@myahrens.de>
2022-09-20 16:17:20 +0200

    Update AddCookieOptinJsAndCss.php
    fix invalid js url

--- Classes/UserFunction/AddCookieOptinJsAndCss.php
+++ Classes/UserFunction/AddCookieOptinJsAndCss.php
@@ -105,10 +105,10 @@ class AddCookieOptinJsAndCss implements SingletonInterface {
 				$overwrittenBaseUrl = $jsonData['settings']['overwrite_baseurl'];
 			}
 
-			$fileUrl = $overwrittenBaseUrl ?? $siteBaseUrl . $file . '?' . $cacheBuster;
+			$siteBaseUrl = $overwrittenBaseUrl ?? BaseUrlService::getSiteBaseUrl($this->rootpage);
 			return '<script id="cookieOptinData" type="application/json">' . json_encode($jsonData) . '</script>
-					<link rel="preload" as="script" href="' . $fileUrl . '" data-ignore="1" crossorigin="anonymous">
-					<script src="' . $fileUrl . '" data-ignore="1" crossorigin="anonymous"></script>';
+					<link rel="preload" as="script" href="' . $siteBaseUrl . $file . '?' . $cacheBuster . '" data-ignore="1" crossorigin="anonymous">
+					<script src="' . $siteBaseUrl . $file . '?' . $cacheBuster . '" data-ignore="1" crossorigin="anonymous"></script>';
 		}
 	}
 
